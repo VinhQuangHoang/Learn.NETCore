f3d108242de7fc1e1e6156c1d024b436fc198fdc
hoangvinhquang69@gmail.com <hoangvinhquang69@gmail.com>
2020-10-11 11:49:46 +0700

    commit AddRazorView and IndexRazorView

--- Models/ProductViewModel.cs
+++ Models/ProductViewModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BT_Tuan1.Models
+namespace ProductManagement.Models
 {
     public class ProductViewModel
     {
