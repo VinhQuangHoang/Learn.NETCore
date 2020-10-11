1be907b16346a4c0a148f3c38f6da0d297afcf8f
hoangvinhquang69@gmail.com <hoangvinhquang69@gmail.com>
2020-10-11 11:34:50 +0700

    commit ProductContrller and ProductViewModel

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BT_Tuan1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BT_Tuan1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public List<ProductViewModel> ListProduct { get; set; } = new List<ProductViewModel>
+            {
+                new ProductViewModel
+                {
+                    Id = 1,
+                    Name = "Iphone",
+                    Color = "Red",
+                    ImageUrl = "",
+                    Amount = 20
+                },
+
+                new ProductViewModel
+                {
+                    Id = 2,
+                    Name = "SamSung",
+                    Color = "blue",
+                    ImageUrl = "",
+                    Amount = 10
+                },
+
+                new ProductViewModel
+                {
+                    Id = 3,
+                    Name = "Oppo",
+                    Color = "White",
+                    ImageUrl = "",
+                    Amount = 15
+                }
+            };
+        public IActionResult Index()
+        {
+            // write name of view model => Ctrl + . -> Enter or choose
+
+            return View(ListProduct);
+        }
+        public IActionResult Add()
+        {
+            var model = new ProductViewModel();
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public IActionResult Add(ProductViewModel model)
+        {
+            ListProduct.Add(model);
+            return View("Index", ListProduct);
+        }
+    }
+}
---
+++ Models/ProductViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BT_Tuan1.Models
+{
+    public class ProductViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(10)]
+
+        public string Color { get; set; }
+        public string ImageUrl { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int Amount { get; set; }
+        // prop => tab 2 times
+    }
+}
